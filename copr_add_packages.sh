b494abf2ac63c20d0adeaee5e9f7098ba5036d3d
xambroz <723625+xambroz@users.noreply.github.com>
2023-11-26 22:09:30 +0100

    helper script

---
+++ copr_add_packages.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PACKAGE=$1
+
+find "./$PACKAGE" -maxdepth 1 -type d | grep -v -E '^./$|^./.git' | \
+while read I ; do
+    PACKAGE=$(basename "$I")
+    grep -i "NoSource:" "${I}/${PACKAGE}.spec"
+    if [ $? -eq 0 ] ; then
+        # Do not add to COPR the binary packages
+        continue
+    fi
+
+    copr add-package-scm --clone-url "https://github.com/xambroz/rpms-misc" \
+        --subdir "${PACKAGE}" \
+        --spec "${PACKAGE}.spec" \
+        --type git \
+        --method make_srpm \
+        --name "${PACKAGE}" \
+        --webhook-rebuild on \
+        rebus/misc
+
+    # package probably already exists, fix parameters
+    if [ $? -ne 0 ] ; then
+        copr edit-package-scm --clone-url "https://github.com/xambroz/rpms-misc" \
+        --subdir "${PACKAGE}" \
+        --spec "${PACKAGE}.spec" \
+        --type git \
+        --method make_srpm \
+        --name "${PACKAGE}" \
+        --webhook-rebuild on \
+        rebus/misc
+    fi
+
+    copr build-package rebus/misc --name "${PACKAGE}"
+done
